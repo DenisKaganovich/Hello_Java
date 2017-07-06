07290117504709d00677b589554db2d3cd441a3c
Denis_Kaganovich <Denis_Kaganovich@epam.com>
2017-07-06 20:21:19 +0300

    project structure of "Hello Java"

--- src/main/java/dekan/hello/java/SquareEquation.java
+++ src/main/java/dekan/hello/java/SquareEquation.java
@@ -4,9 +4,17 @@ package dekan.hello.java;
  * Created by Denis_Kaganovich on 7/4/2017.
  */
 public class SquareEquation {
-
+//    double a,b,c;
+//    a=1;
+//    b=3;
+//    c=5;
     public static void main(String[] args) {
         System.out.println("..:: SQUARE EQUATION ::..");
+        int a,b,c;
+//    a=1;
+//    b=3;
+//    c=5;
+        System.out.println("a=" + a + "\n" +"b=" + b + "\n" +"c=" + c);
     }
 
 }
