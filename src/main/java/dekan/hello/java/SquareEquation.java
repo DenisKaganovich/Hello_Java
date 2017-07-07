9ce6e9c00bfd4055e028ea2f8b9d833684d5d5c9
Denis_Kaganovich <Denis_Kaganovich@epam.com>
2017-07-07 14:21:36 +0300

    Task 1. ver.1

--- src/main/java/dekan/hello/java/SquareEquation.java
+++ src/main/java/dekan/hello/java/SquareEquation.java
@@ -1,5 +1,5 @@
 package dekan.hello.java;
-
+import java.util.Scanner;
 /**
  * Created by Denis_Kaganovich on 7/4/2017.
  */
@@ -10,11 +10,43 @@ public class SquareEquation {
 //    c=5;
     public static void main(String[] args) {
         System.out.println("..:: SQUARE EQUATION ::..");
-        int a,b,c;
+//        double a,b,c;
 //    a=1;
 //    b=3;
 //    c=5;
-        System.out.println("a=" + a + "\n" +"b=" + b + "\n" +"c=" + c);
-    }
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите значени a: ");
+        double a = in.nextDouble();
+        System.out.print("Введите значени b: ");
+        double b = in.nextDouble();
+        System.out.print("Введите значени c: ");
+        double c = in.nextDouble();
 
-}
+        System.out.println("a=" + a + "\n" +"b=" + b + "\n" +"c=" + c);
+
+        double d;
+        double x;
+        double x1;
+        double x2;
+
+        d = Math.pow(b, 2) - (4*a*c);
+
+        if (d < 0)
+        {
+            System.out.print("Вещественных корней нет, т.к. D = " + d);
+                    }
+        else {
+          if (d == 0)
+          {
+              x = -b/(2*a);
+              System.out.print("Один корень, т.к. D = 0" + "\n"+ "X = " + x);
+                      }
+          else
+          {
+              x1=(-b-Math.sqrt(d))/(2*a);
+              x2=(-b+Math.sqrt(d))/(2*a);
+              System.out.print("Два корня, т.к. D = " + d + "\n"+ "Первый вещественный корень X1 = " + x1 + "\n" + "Второй вещественный корень X2 = " + x2);
+          }
+        }
+        }
+    }
